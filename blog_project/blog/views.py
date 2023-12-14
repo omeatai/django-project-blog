58c0da98e47dd417f9e7918351cd3bc137b5b10d
omeatai <omeatai@gmail.com>
2023-12-14 08:43:48 -0700

    12. Setup Urls and Views
    12. Setup Urls and Views

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    """
+    This is the home view function.
+    It returns an HTTP response.
+    """
+    return HttpResponse("<h1>Hello, Django!</h1>")
+    # return render(request, 'home.html', {})
+
+def posts(request):
+    """
+    This is the posts view function.
+    It returns an HTTP response.
+    """
+    return HttpResponse("<h1>Hello, Posts Page!</h1>")
+
+def posts_detail(request, slug):
+    """
+    This is the posts_detail view function.
+    It returns an HTTP response.
+    """
+    return HttpResponse(f"<h1>Hello, Post Detail Page! - {slug}</h1>")
