738b6d4797f9d39e4a9d3fc2789b59115f1fd064
omeatai <omeatai@gmail.com>
2023-12-17 22:59:24 -0700

    22. Book Store Demo Project: Model Include Validators
    22. Book Store Demo Project: Model Include Validators

--- book_store_project/books/models.py
+++ book_store_project/books/models.py
@@ -1,11 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_not_a_number(value):
+    if not str(value).isdigit():
+        raise ValidationError(
+            _("%(value)s is not a number!"),
+            params={"value": value},
+        )
 
 # Create your models here.
 
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[validate_not_a_number, MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(blank=True, null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
     # id = models.AutoField(primary_key=True)
     # author = models.CharField(max_length=100)
     # price = models.DecimalField(max_digits=5, decimal_places=2)
