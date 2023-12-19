6f10de2c77dc6418449b9b526442f61467816356
omeatai <omeatai@gmail.com>
2023-12-19 14:29:17 -0700

    35. Book Store Demo Project: Setup Aggregation & Ordering
    35. Book Store Demo Project: Setup Aggregation & Ordering

--- book_store_project/books/views.py
+++ book_store_project/books/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
-
+from django.db.models import Avg, Min, Max, Count, Sum
 from .models import Book
 
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()  # Retrieve all books from the database
-    # print(books)
-    return render(request, "books/index.html", {"books": books})
+    books = Book.objects.all().order_by("-rating","title")  # Retrieve all books and Order from the database
+    num_books = books.count()  # Count the number of books
+    aggregated_rating = books.aggregate(average_rating=Avg("rating"), min_rating=Min("rating"), max_rating=Max("rating"), count_rating=Count("rating"), sum_rating=Sum("rating"))  # Calculate on the rating
+
+    return render(request, "books/index.html", {
+        "books": books,
+        "total_number_of_books": num_books,
+        "aggregated_rating": aggregated_rating,
+    })
 
 
 def book_details(request, slug):
