bfb0edf46c325579fb03037ace7a8188e6a3ad74
omeatai <omeatai@gmail.com>
2023-12-18 19:26:59 -0700

    30. Book Store Demo Project: Rendering Queried Data in Template
    30. Book Store Demo Project: Rendering Queried Data in Template

--- book_store_project/books/views.py
+++ book_store_project/books/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "books/index.html", {})
+    books = Book.objects.all()  # Retrieve all books from the database
+    print(books)
+    return render(request, "books/index.html", {"books": books})
