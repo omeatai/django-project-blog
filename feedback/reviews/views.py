736d838d864b6cf1773875f035f8bfac6d22e12b
omeatai <omeatai@gmail.com>
2023-12-24 19:01:22 -0700

    51. Feedback Demo Project: Setup Template View
    51. Feedback Demo Project: Setup Template View

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from .forms import ReviewForm
 from .models import Review
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
@@ -25,25 +26,38 @@ class ReviewView(View):
         })
 
 
-# def review(request):
-#     if request.method == 'POST':
-#         form = ReviewForm(request.POST)
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
-#         # This is a way to update an existing review.
-#         # existing_data = Review.objects.get(pk=1)
-#         # form = ReviewForm(request.POST, instance=existing_data)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "Thank you for your review!"
+        return context
 
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect("/thank-you")
+# class ThankYouView(View):
+#     def get(self, request):
+#         return render(request, "reviews/thank_you.html")
 
-#     else:
-#         form = ReviewForm()
-
-#     return render(request, "reviews/review.html", {
-#         "form": form
-#     })
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
+        return context
+
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(id=review_id)
+        context["review"] = selected_review
+        return context
