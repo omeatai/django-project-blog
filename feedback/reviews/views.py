6e2ab6f2c52147d4988f4bae4975fd5f553a51f3
omeatai <omeatai@gmail.com>
2023-12-24 21:10:24 -0700

    54. Feedback Demo Project: Setup Form View
    54. Feedback Demo Project: Setup Form View

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -5,26 +5,36 @@ from .forms import ReviewForm
 from .models import Review
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
+
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
+
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
 
 
 class ThankYouView(TemplateView):
