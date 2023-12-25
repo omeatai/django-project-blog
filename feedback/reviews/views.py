956cad3b3e5eaa8c2cd9ad0e5da9fb13f158355d
omeatai <omeatai@gmail.com>
2023-12-24 20:45:12 -0700

    52. Feedback Demo Project: Setup List View
    52. Feedback Demo Project: Setup List View

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from .forms import ReviewForm
 from .models import Review
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -42,14 +43,25 @@ class ThankYouView(TemplateView):
 #     return render(request, "reviews/thank_you.html")
 
 
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
+    model = Review
     template_name = "reviews/review_list.html"
+    context_object_name = "reviews"
+    ordering = ["-rating"]
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=1)
+        return data
+
+# class ReviewsListView(TemplateView):
+#     template_name = "reviews/review_list.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         reviews = Review.objects.all()
+#         context["reviews"] = reviews
+#         return context
 
 
 class SingleReviewView(TemplateView):
