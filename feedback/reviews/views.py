8742a736a01c27130d798e633da50e32948fb998
omeatai <omeatai@gmail.com>
2023-12-24 18:02:53 -0700

    50. Feedback Demo Project: Class Based Views
    50. Feedback Demo Project: Class Based Views

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,29 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
 
-def review(request):
-    if request.method == 'POST':
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
-
-        # This is a way to update an existing review.
-        # existing_data = Review.objects.get(pk=1)
-        # form = ReviewForm(request.POST, instance=existing_data)
-
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/thank-you")
 
-    else:
-        form = ReviewForm()
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+
+# def review(request):
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST)
+
+#         # This is a way to update an existing review.
+#         # existing_data = Review.objects.get(pk=1)
+#         # form = ReviewForm(request.POST, instance=existing_data)
+
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
+
+#     else:
+#         form = ReviewForm()
+
+#     return render(request, "reviews/review.html", {
+#         "form": form
+#     })
 
 
 def thank_you(request):
